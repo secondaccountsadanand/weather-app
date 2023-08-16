15c28a28058478e0220cd23aa5a32bb4aded2dcd
Sadanand Miskin <miskinsadanand@gmail.com>
2023-08-16 18:00:00 +0530

    update

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express')
 const cors = require('cors')
 const axios = require('axios')
+require('dotenv').config();
+
 const app = express()
 app.use(express.json())
 app.use(express.urlencoded({extended:true}))
