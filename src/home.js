e183f22a745200463718caa12f63c80ad3ad16a7
Sadanand Miskin <miskinsadanand@gmail.com>
2023-08-16 16:06:38 +0530

    update

--- src/home.js
+++ src/home.js
@@ -12,7 +12,7 @@ export default function Home() {
     let submit = async (e) => {
         e.preventDefault();
         try {
-            const response = await axios.post("vocal-elf-4cdcaf.netlify.app/", {
+            const response = await axios.post(`${url}`, {
                 msg
             });
             setServerResponse(response.data);
